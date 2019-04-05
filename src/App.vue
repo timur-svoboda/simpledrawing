233bd9136e380b9a2cf95735f17aa3632063b221
Timur Svoboda <svobodatimur@gmail.com>
2019-04-05 15:56:32 +0300

    UI of tool panel created

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <tool-panel></tool-panel>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ToolPanel from "./components/ToolPanel.vue";
 
 export default {
-  name: "app",
+  name:"app",
   components: {
-    HelloWorld
+    ToolPanel
   }
 };
 </script>
+
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+html
+  overflow hidden
+
+body
+  margin 0
 </style>
---
+++ src/components/ToolPanel.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav id="tool-panel" class="tool-panel">
+    <div class="tool-panel__tool-item tool-item">
+      <img
+        src="../assets/move.svg"
+        alt="move-tool-icon"
+        class="tool-item__tool-icon tool-icon"
+      />
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "tool-panel"
+};
+</script>
+
+<style lang="stylus" scoped>
+.tool-panel
+  display flex
+  align-items center
+  padding 15px 20px
+  box-shadow: 0 5px 10px 0 rgba(0,0,0,0.2)
+  
+.tool-panel__tool-item
+  display flex
+  align-items center
+  margin-right 10px
+
+.tool-item__tool-icon
+  display inline-block
+  height auto
+  width 24px
+  
+  
+</style>
